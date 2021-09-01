1e406c958a2933e714c4a14124cd961cd221334a
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-09-01 12:42:17 +0200

    Initial commit

---
+++ src/components/MoviesList.js
@@ -0,0 +1,50 @@
+import React, { useContext } from "react";
+import MovieContext from "../contexts/MovieContext";
+
+function MoviesList() {
+  const { movies, setMovies } = useContext(MovieContext);
+
+  function deleteMovie(id) {
+    const filteredMovies = movies.filter((movie) => movie.movie_id !== id);
+    setMovies(filteredMovies);
+  }
+  function restoreMovie() {
+    setMovies([
+      {
+        movie_id: 1,
+        title: "Harry Potter and the Sorcerers Stone",
+        release_year: 2001
+      },
+      {
+        movie_id: 2,
+        title: "Harry Potter and the Chamber of Secrets",
+        release_year: 2002
+      },
+      {
+        movie_id: 3,
+        title: "Harry Potter and the Prison of Azkaban",
+        release_year: 2004
+      }
+    ]);
+  }
+
+  return (
+    <>
+      <h1>Movies List</h1>
+      {movies.map((movie) => (
+        <div className="movie" id={movie.movie_id}>
+          ★ {movie.title} &nbsp;
+          <button type="button" onClick={() => deleteMovie(movie.movie_id)}>
+            x
+          </button>
+        </div>
+      ))}
+      <br />
+      <button type="button" onClick={() => restoreMovie()}>
+        Reset
+      </button>
+    </>
+  );
+}
+
+export default MoviesList;
---
+++ src/contexts/MovieContext.js
@@ -0,0 +1,6 @@
+// create a MovieContext variable for our context, setting its initial value to null. don't forget to import createContext from react!
+import { createContext } from "react";
+
+const MovieContext = createContext(null);
+
+export default MovieContext;
